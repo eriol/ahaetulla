e87d4c3ae85a188116bc4bf21bbd0b030833023e
Daniele Tricoli <eriol@mornie.org>
2023-02-28 20:34:28 +0100

    Add initial implementation of a send command

--- ble/ble.go
+++ ble/ble.go
@@ -8,7 +8,7 @@ import (
 	"tinygo.org/x/bluetooth"
 )
 
-// Scan for BLE devices for the specified time t.
+// Scan for Bluetooth LE devices for the specified time t.
 //
 // Devices are returned in descendind RSSI order.
 func ScanUntilTimeout(t float32) []bluetooth.ScanResult {
@@ -50,4 +50,67 @@ func ScanUntilTimeout(t float32) []bluetooth.ScanResult {
 	sort.Slice(devices, func(i, j int) bool { return devices[i].RSSI > devices[j].RSSI })
 
 	return devices
+}
+
+// Return a specific device from its address.
+// TODO: right now it's unbounded, ad a max timeout.
+func FindDeviceByAddress(address string) bluetooth.ScanResult {
+	adapter := bluetooth.DefaultAdapter
+
+	err := adapter.Enable()
+	if err != nil {
+		log.Error(err)
+	}
+
+	ch := make(chan bluetooth.ScanResult, 1)
+
+	err = adapter.Scan(func(adapter *bluetooth.Adapter, result bluetooth.ScanResult) {
+		if result.Address.String() == address {
+			adapter.StopScan()
+			ch <- result
+		}
+	})
+	if err != nil {
+		log.Error(err)
+	}
+
+	return <-ch
+}
+
+func Send(address, text string) {
+	adapter := bluetooth.DefaultAdapter
+
+	err := adapter.Enable()
+	if err != nil {
+		log.Error(err)
+	}
+
+	r := FindDeviceByAddress(address)
+
+	var device *bluetooth.Device
+
+	device, err = adapter.Connect(r.Address, bluetooth.ConnectionParams{})
+	if err != nil {
+		log.Error(err)
+	}
+	services, err := device.DiscoverServices([]bluetooth.UUID{bluetooth.ServiceUUIDNordicUART})
+	if err != nil {
+		log.Error("Failed to discover the Nordic UART Service: ", err)
+	}
+	service := services[0]
+	chars, err := service.DiscoverCharacteristics(
+		[]bluetooth.UUID{bluetooth.CharacteristicUUIDUARTRX, bluetooth.CharacteristicUUIDUARTTX})
+	if err != nil {
+		log.Error("Failed to discover RX and TX characteristics: ", err)
+	}
+	rx := chars[0]
+	// tx := chars[1]
+
+	// FIXME: this fails sometime.
+	_, err = rx.WriteWithoutResponse([]byte(text))
+	if err != nil {
+		log.Error("Could not send: ", err)
+	}
+
+	device.Disconnect()
 }
---
+++ cli/send.go
@@ -0,0 +1,27 @@
+package cli
+
+import (
+	"strings"
+
+	"github.com/spf13/cobra"
+	"noa.mornie.org/eriol/ahaetulla/ble"
+)
+
+var address string
+
+var sendCmd = &cobra.Command{
+	Use:   "send",
+	Short: "Send one or more words over BLE to a specific device.",
+	Long:  "",
+	Args:  cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		text := strings.Join(args, " ")
+		ble.Send(address, text)
+	},
+}
+
+func init() {
+	sendCmd.Flags().StringVar(&address, "device", "", "ble device address")
+	sendCmd.MarkFlagRequired("device")
+	rootCmd.AddCommand(sendCmd)
+}
